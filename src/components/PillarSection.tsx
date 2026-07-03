50574c2bf38603539b5b1b5288b52c9c072cdda7
Yin Aoqi <leanhdep301121@gmail.com>
2026-07-04 01:43:22 +0800

    Remove second screen animation

--- src/components/PillarSection.tsx
+++ src/components/PillarSection.tsx
@@ -1,6 +1,3 @@
-import { motion } from 'motion/react';
-import LightPillar from './LightPillar';
-import WebGLBoundary from './WebGLBoundary';
 import type { useParallax } from './useParallax';
 
 type PillarSectionProps = {
@@ -8,32 +5,9 @@ type PillarSectionProps = {
   onScrollPrev: () => void;
 };
 
-export default function PillarSection({ parallax, onScrollPrev }: PillarSectionProps) {
+export default function PillarSection({ onScrollPrev }: PillarSectionProps) {
   return (
     <section className="relative flex h-[100dvh] w-full shrink-0 items-center justify-center overflow-hidden bg-black">
-      <motion.div
-        className="absolute inset-0 z-0"
-        style={{ scale: parallax.pillarBg.scale, y: parallax.pillarBg.y, filter: parallax.pillarBg.filter }}
-      >
-        <WebGLBoundary>
-          <LightPillar
-            topColor="#f2f6fa"
-            bottomColor="#94a3b8"
-            intensity={0.72}
-            rotationSpeed={0.4}
-            glowAmount={0.0022}
-            pillarWidth={8.8}
-            pillarHeight={0.16}
-            noiseIntensity={0.28}
-            pillarRotation={18}
-            interactive={false}
-            mixBlendMode="normal"
-            quality="high"
-            className="opacity-95 brightness-110 contrast-125"
-          />
-        </WebGLBoundary>
-      </motion.div>
-
       <button
         type="button"
         onClick={onScrollPrev}
