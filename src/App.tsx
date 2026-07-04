f337f12e93b973d30d5bdf0ef66f06c3a0bf78dc
Yin Aoqi <leanhdep301121@gmail.com>
2026-07-04 11:54:36 +0800

    Disable scroll to second page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { motion, useReducedMotion, useTransform } from 'motion/react';
 import type { MotionValue } from 'motion/react';
 import Nav from './components/Nav';
 import Hero from './components/Hero';
-import PillarSection from './components/PillarSection';
 import { useParallax } from './components/useParallax';
 
 export default function App() {
@@ -14,9 +13,8 @@ export default function App() {
   return (
     <div className="h-[100dvh] overflow-hidden bg-black text-white">
       <Nav />
-      <main ref={mainRef} className="h-[100dvh] overflow-y-scroll">
+      <main ref={mainRef} className="h-[100dvh] overflow-hidden">
         <Hero parallax={parallax} />
-        <PillarSection parallax={parallax} />
       </main>
 
       <Scrim progress={parallax.progress} />
